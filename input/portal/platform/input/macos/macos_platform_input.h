b8a83697bc680d9b91afc20f35fa65643ed17b77
Jonatan Nevo <jonatannevo-git@proton.me>
2025-07-30 16:43:58 +0300

    (input): add mac support

---
+++ input/portal/platform/input/macos/macos_platform_input.h
@@ -0,0 +1,15 @@
+//
+// Copyright © 2025 Jonatan Nevo.
+// Distributed under the MIT license (see LICENSE file).
+//
+
+#pragma once
+
+#include "portal/platform/input/generic/generic_platform_input.h"
+
+namespace portal::input
+{
+
+typedef GenericPlatformInput PlatformInput;
+
+} // portal
